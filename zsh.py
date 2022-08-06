3f0b5f4d29134dc1a18fe2d2dd31fe4275c5e0b3
CJYLZS <691086891@qq.com>
2022-08-06 17:58:31 +0800

    update

--- zsh.py
+++ zsh.py
@@ -6,9 +6,9 @@ def run(cmd : str):
     print( sb.getoutput(cmd))
 
 
-run('git clone --depth=1 git@github.com:ohmyzsh/ohmyzsh.git ~/.oh-my-zsh')
-run('git clone --depth=1 git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/')
-run('git clone --depth=1 git@github.com:zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/')
+run('git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh')
+run('git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/')
+run('git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/')
 
 run('cp ylzs.zsh-theme ~/.oh-my-zsh/themes/')
 run('cp -f dirhistory.plugin.zsh ~/.oh-my-zsh/plugins/dirhistory/')
