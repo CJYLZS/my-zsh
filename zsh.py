7037d8899ba58965ec05145a3775c55f855590dc
CJYLZS <691086891@qq.com>
2022-08-06 17:35:48 +0800

    update

---
+++ zsh.py
@@ -0,0 +1,19 @@
+import os
+import subprocess as sb
+
+def run(cmd : str):
+    print('run', cmd)
+    print( sb.getoutput(cmd))
+
+
+run('git clone --depth=1 git@github.com:ohmyzsh/ohmyzsh.git ~/.oh-my-zsh')
+run('git clone --depth=1 git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/')
+run('git clone --depth=1 git@github.com:zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/')
+
+run('cp ylzs.zsh-theme ~/.oh-my-zsh/themes/')
+run('cp -f dirhistory.plugin.zsh ~/.oh-my-zsh/plugins/dirhistory/')
+try:
+    run('mv ~/.zshrc ~/.zshrc.bak')
+except:
+    pass
+run('cp .zshrc ~/.zshrc')
--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,8 @@
 set -x
-git clone --depth=1 git@github.com:ohmyzsh/ohmyzsh.git -d ~/ &&\
-git clone --depth=1 git@github.com:zsh-users/zsh-syntax-highlighting.git -d ~/.oh-my-zsh/custom/plugins/ &&\
-git clone --depth=1 git@github.com:zsh-users/zsh-autosuggestions.git -d ~/.oh-my-zsh/custom/plugins/ &&\
-cp ylzs.zsh-theme ~/.oh-my-zsh/theme/ &&\
-cp -f dirhistory.plugin.zsh ~/.oh-my-zsh/plugin/dirhistory/
+git clone --depth=1 git@github.com:ohmyzsh/ohmyzsh.git ~/.oh-my-zsh/ &&\
+git clone --depth=1 git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting.git &&\
+git clone --depth=1 git@github.com:zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&\
+cp ylzs.zsh-theme ~/.oh-my-zsh/themes/ &&\
+cp -f dirhistory.plugin.zsh ~/.oh-my-zsh/plugins/dirhistory/
 mv ~/.zshrc ~/.zshrc.bak &&\
-cp ~/.zshrc ~/.zshrc &&\
+cp ~/.zshrc ~/.zshrc 
